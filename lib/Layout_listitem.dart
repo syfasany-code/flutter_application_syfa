458acded96d1dcb51a34dfe9b17ee8edba330e24
nisyasiti26-lang <nisyasiti26@gmail.com>
2025-09-22 16:06:44 +0700

    menambahkan tugas

---
+++ lib/Layout_listitem.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+// Abstract class untuk item list
+abstract class ListItem {}
+
+// Heading Item
+class HeadingItem implements ListItem {
+  final String heading;
+  HeadingItem(this.heading);
+}
+
+// Message Item
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+  MessageItem(this.sender, this.body);
+}
+
+// Image Item
+class ImageItem implements ListItem {
+  final String title;
+  final String imagePath;
+  ImageItem(this.title, this.imagePath);
+}
+
+class ListExample extends StatelessWidget {
+  const ListExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Data contoh untuk ImageItem
+    final imageItems = [
+      ImageItem("Gunung", "images/gunung.jpeg"),
+      ImageItem("Pantai", "images/pantai.jpeg"),
+      ImageItem("Kota", "images/kota.jpeg"),
+    ];
+
+    // Buat list berisi 30 item
+    final items = List<ListItem>.generate(30, (i) {
+      if (i % 5 == 0) {
+        return HeadingItem("Heading $i");
+      } else if (i % 2 == 0) {
+        return MessageItem("Pengirim $i", "Pesan ke-$i");
+      } else {
+        // Ambil imageItem secara bergantian
+        return imageItems[i % imageItems.length];
+      }
+    });
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Latihan ListView Flutter')),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          if (item is HeadingItem) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                item.heading,
+                style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          } else if (item is MessageItem) {
+            return ListTile(
+              leading: const Icon(Icons.message, color: Colors.green),
+              title: Text(item.sender),
+              subtitle: Text(item.body),
+            );
+          } else if (item is ImageItem) {
+            return Card(
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    item.imagePath,
+                    fit: BoxFit.cover,
+                    height: 200,
+                    width: double.infinity,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      item.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_syfa/latihan2.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: "Aplikasi Flutter",
+      home: ListExample(), // halaman utama
     );
   }
 }
