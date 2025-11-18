cfe3ccf5837192f99ea5d28e1ad4796ef8836647
Yasyfa Hidayanti <syfasany@gmail.com>
2025-11-18 10:55:25 +0700

    menambahkan state

---
+++ lib/latihanstateful.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Latihanstate extends StatefulWidget {
+  const Latihanstate({super.key});
+
+  @override
+  State<Latihanstate> createState() => _LatihanstateState();
+}
+
+class _LatihanstateState extends State<Latihanstate> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Latihan State')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Nilai Counter saat ini: $counter'),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: incrementCounter,
+              child: const Text('Tambah Counter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_syfa/latihan_naviagator.dart';
+import 'package:flutter_application_syfa/latihanstateful.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Latihan Navigator",
-      home: LatihanNavigator(), // halaman utama
+      home: Latihanstate(), // halaman utama
     );
   }
 }
